1eb03b4167c9dc6224f57829fba5003bf12597fa
seeduardo <romenflatiron@gmail.com>
2019-03-04 17:48:10 +0000

    Lab completed

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -6,11 +6,18 @@ function SomeDirective() {
 				'<span>Click here to replace the text</span>',
 			'</div>'
 		].join(''),
+		require: 'someDirective',
 		controller: function() {
 			this.text = 'Replace this text!';
 		},
 		controllerAs: 'some',
-		link: function (scope, elem, attrs) {
+		link: function (scope, elem, attrs, ctrl) {
+			let wholeSomeEl = elem[0]
+			let someTextEl = wholeSomeEl.querySelector('span')
+			someTextEl.addEventListener('click', () => {
+				ctrl.text = 'Thank you for clicking!'
+				scope.$apply()
+			})
 
 		}
 	}
